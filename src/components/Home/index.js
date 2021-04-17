73872b6bff3723b73ace67fa9b151ecc4fedffaf
tetolivares <tetolivares@gmail.com>
2021-04-17 00:25:15 -0400

    transition to inputTypeFile

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,38 @@
-const Home = () => (
-  <div className="h-96 w-screen text-center flex justify-center flex-col">
-    This is the Home
-  </div>
-)
+const Home = () => {
 
+  return (
+    <div className="h-96 p-12 w-screen text-center flex justify-center flex-col">
+      <input
+        className="my-2 p-4 bg-gray-300"
+        name="title"
+        placeholder="title"
+      />
+      <input
+        className="my-2 p-4 bg-gray-300"
+        name="price"
+        placeholder="price"
+      />
+      <label for="file" className="bg-red-500 m-5 p-5 hover:transform hover:scale-105 transition-all duration-150S">
+        selecciona una imagen
+      </label>
+      <input
+        id="file"
+        className="hidden my-2 p-4 bg-gray-300"
+        name="image"
+        placeholder="image"
+        type="file"
+      />
+      <select
+        className="my-2 p-4 bg-gray-300"
+        name="image"
+        placeholder="image"
+        type="file"
+      >
+        <option>blusa</option>
+        <option>pantalon</option>
+        <option>vestido</option>
+      </select>
+    </div>
+  )
+}
 export default Home
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,12 @@
+
+import { UserIcon } from '@heroicons/react/solid'
+
 const Footer = () => (
-  <footer className="fixed bottom-0 bg-green-50 py-2 w-full text-center font-light text-red-800 font-light">
-    Tiendi Footer
+  <footer className="flex justify-around fixed bottom-0 bg-green-50 py-2 w-full text-center font-light text-red-800 font-light">
+    <UserIcon className="h-5 w-5 text-blue-500"/>
+    <UserIcon className="h-5 w-5 text-blue-500"/>
+    <UserIcon className="h-5 w-5 text-blue-500"/>
+    <UserIcon className="h-5 w-5 text-blue-500"/>
   </footer>
 )
 
