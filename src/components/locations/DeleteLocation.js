6466b17884f3e050c8d7dd6cb50c27558f3fece5
julight <jperdomocorrea@gmail.com>
2021-05-21 08:37:54 -0400

    Locations CRUD Delete action

--- src/components/locations/DeleteLocation.js
+++ src/components/locations/DeleteLocation.js
@@ -1,8 +1,19 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
+import { useFirestore } from 'react-redux-firebase'
 import DeleteModal from '@components/shared/Modal'
 const DeleteLocation = ({ isOpen }) => {
   const history = useHistory()
+  const firestore = useFirestore()
+  const { id } = useParams()
   const handleCloseModal = () => {
+    history.push('/backoffice/locations')
+  }
+  const handleDeleteLocation = async () => {
+    console.log('About to delete location: ' + id)
+    const deleteLocationResponse = await firestore.collection('locations')
+      .doc(id)
+      .delete()
+    console.info(deleteLocationResponse)
     history.push('/backoffice/locations')
   }
   return (
@@ -11,8 +22,16 @@ const DeleteLocation = ({ isOpen }) => {
         <div className="flex flex-col justify-center h-full text-center">
           <span className="mb-6 font-bold text-2xl">¿Estás seguro de borrar este lugar?</span>
           <div className="flex justify-center">
-            <button className="px-8 py-2 rounded-lg mx-4 bg-green-600 text-white font-bold">Si</button>
-            <button onClick={handleCloseModal} className="px-8 py-2 rounded-lg bg-red-600 text-white font-bold">No</button>
+            <button
+              className="px-8 py-2 rounded-lg mx-4 bg-green-600 text-white font-bold"
+              onClick={handleDeleteLocation}
+            >Si
+            </button>
+            <button
+              className="px-8 py-2 rounded-lg bg-red-600 text-white font-bold"
+              onClick={handleCloseModal}
+            >No
+            </button>
           </div>
         </div>
       </DeleteModal>
