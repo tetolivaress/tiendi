e588a0e627b3babef275cb0e83593b3250047c2a
julight <jperdomocorrea@gmail.com>
2021-06-07 14:51:07 -0400

    Add delivery locations to menu

--- src/pages/backoffice/index.js
+++ src/pages/backoffice/index.js
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom"
 
 const Backoffice = () => (
-  <Link to="categories" className="shadow m-8 p-4">
-    Categorías del sitio
-  </Link>
+  <>
+    <Link to="categories" className="shadow m-8 p-4">
+      Categorías del sitio
+    </Link>
+    <Link to="categories" className="shadow m-8 p-4">
+      Lugares de entrega
+    </Link>
+  </>
 )
 
 export default Backoffice
